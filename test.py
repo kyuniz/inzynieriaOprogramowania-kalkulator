e7f48227f29a9f801a200f5fb883d75a327bddca
Tomasz Kunicki <tomekkunicki7@gmail.com>
2022-04-02 22:32:24 +0200

    dodano kalkulator z dodawaniem

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
+
+
 # To bedzie plik do pracy z repo
 def hello(name):
 	return "Hello" + str(name)
 
-print ("Hello world")
+def dodaj (a,b):
+	wynik = float(a) + float(b)
+	return wynik
+
+pierwsza = input()
+druga = input ()
+
+print (dodaj(pierwsza, druga))
+
+
